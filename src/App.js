7bc244fe7f2de9045c9aa91048c3ac6e438653c6
Neil Devas <neil.devas97@gmail.com>
2020-10-18 19:19:15 -0400

    Add mutation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
 import LinkList from "./components/LinkList";
+import CreateLink from "./components/CreateLink";
 
 function App() {
   return (
-    <LinkList />
+    // <LinkList />
+      <CreateLink />
   );
 }
 
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 import Link from './Link'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag';
 
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
 class LinkList extends Component {
   render() {
     const linksToRender = [
@@ -14,10 +28,23 @@ class LinkList extends Component {
         description: 'The best GraphQL client',
         url: 'https://www.apollographql.com/docs/react/',
       },
-    ]
+    ];
 
     return (
-        <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+
+          const linksToRender = data.feed.links;
+
+          return (
+              <div>
+                {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              </div>
+          )
+        }}
+      </Query>
     )
   }
 }
