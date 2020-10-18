f909be07ec940f4ed16eba230c6615e2daf9fbe2
Neil Devas <neil.devas97@gmail.com>
2020-10-18 19:00:24 -0400

    Implement static components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/App.css';
+import LinkList from "./components/LinkList";
+
+function App() {
+  return (
+    <LinkList />
+  );
+}
+
+export default App;
